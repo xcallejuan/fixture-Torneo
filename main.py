4ae7a3b3efb0c5eddb5148c73a4eb5d54e2065af
juan arango <juanesarangocalle9@gmail.com>
2025-09-21 10:47:26 -0500

    Se añadieron métodos para registrar goles y representación de cadena.

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ class Jugador:
         self.edad = edad
         self.posicion = posicion
         self.goles = 0
+
+    def registrar_gol(self):
+        self.goles += 1
+
+    def __str__(self):
+        return f"{self.nombre} ({self.posicion}, {self.edad} años) - Goles: {self.goles}"
